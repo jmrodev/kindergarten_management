c59e3886ad7019cc1c286fbed44d730e28063d15
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2025-12-12 15:49:23 -0300

    chore: add barrel index for components/organisms

---
+++ kindergarten-frontend-vite/src/components/organisms/index.jsx
@@ -0,0 +1,4 @@
+export { default as RibbonLayout } from './RibbonLayout.jsx';
+export { default as OfficeRibbon } from './OfficeRibbon.jsx';
+export { default as OfficeRibbonWithTitle } from './OfficeRibbonWithTitle.jsx';
+export { default as StudentForm } from './StudentForm.jsx';
