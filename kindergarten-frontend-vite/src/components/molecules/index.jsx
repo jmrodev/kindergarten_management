db571f4d02b5580a392dcfcd1b55aa14c4e9026b
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2025-12-13 22:05:51 -0300

    restructiring front

--- kindergarten-frontend-vite/src/components/molecules/index.jsx
+++
@@ -1,7 +0,0 @@
-export { default as Modal } from './Modal.jsx';
-export { default as ConfirmationModal } from './ConfirmationModal.jsx';
-export { default as FormModal } from './FormModal.jsx';
-export { default as TableRow } from './TableRow.jsx';
-export { default as ActionItem } from './ActionItem.jsx';
-export { default as ModuleCard } from './ModuleCard.jsx';
-export { default as RoleFilter } from './RoleFilter.jsx';
--- kindergarten-frontend-vite/src/components/organisms/index.jsx
+++
@@ -1,8 +0,0 @@
-export { default as RibbonLayout } from './RibbonLayout.jsx';
-export { default as OfficeRibbon } from './OfficeRibbon.jsx';
-export { default as OfficeRibbonWithTitle } from './OfficeRibbonWithTitle.jsx';
-export { default as OfficeTable } from './OfficeTable.jsx';
-export { default as VaccinationDetails } from './VaccinationDetails.jsx';
-export { default as StudentForm } from './StudentForm.jsx';
-export { default as AuditCard } from './AuditCard.jsx';
-export { default as RoleCard } from './RoleCard.jsx';
